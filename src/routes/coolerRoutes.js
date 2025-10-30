5ea4fc65136a311cea14d2de0c08403c66da7d0d
Alberto Montes <dav.vazquez1719@gmail.com>
2025-10-30 13:12:39 -0600

    fix

--- src/routes/coolerRoutes.js
+++ src/routes/coolerRoutes.js
@@ -4,5 +4,6 @@ const coolerController = require('../controllers/coolerController');
 const authenticateToken = require('../middlewares/authMiddleware');
 
 router.post('/list', authenticateToken, coolerController.getCoolersAndCameras);
+router.get('/public', coolerController.getCoolersPublic);
 
 module.exports = router;
